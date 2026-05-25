2f156fbc7a791f0fdfd1330c4e336c8db62c3c99
Rafraud <triforce16mail@gmail.com>
2026-05-25 02:48:17 -0600

    update profile image

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['scontent-den2-1.cdninstagram.com', 'upload.wikimedia.org', 'static.wikia.nocookie.net', 'upload.wikimedia.org'],
+    domains: ['scontent-den2-1.cdninstagram.com', 'upload.wikimedia.org', 'static.wikia.nocookie.net', 'upload.wikimedia.org', 'pbs.twimg.com'],
   },
 };
 
