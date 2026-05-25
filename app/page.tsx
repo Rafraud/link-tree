fe3f690529e5a18ca39d6a5c606619a845c20d13
Rashad McKinney <triforce16mail@gmail.com>
2026-05-25 12:51:08 -0600

    Remove title element from Home component
    Removed the title element from the Home component.

--- app/page.tsx
+++ app/page.tsx
@@ -123,7 +123,6 @@ function LinkCard({
 
 export default function Home() {
   return (
-    <title>rafdev</title>
     <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
       <Image
         className="rounded-full"
