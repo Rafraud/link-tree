8616533ce346ebddf908d7ca76a7006c26eaa910
Rafraud <rashadmckinney@outlook.com>
2026-05-28 01:04:20 -0600

    start email box

---
+++ app/form.tsx
@@ -0,0 +1,10 @@
+
+export const ContactForm = () => {
+    return (
+        <form>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email"/>
+            <textarea></textarea>
+        </form>
+    )
+}
--- nav/index.jsx
+++ nav/index.jsx
@@ -1,18 +1,24 @@
 import styles from "../styles/sidebar.module.scss";
-import { motion } from 'framer-motion';
-import { menuSlide } from '../app/anim';
+import { motion } from "framer-motion";
+import { menuSlide } from "../app/anim";
+import { ContactForm } from "../app/form";
 
 export default function index() {
-    return(
-        <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className={styles.menu}>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    <div className={styles.header}>
-                        <p>Contact</p>
-                        {/* Contact form here */}
-                    </div>
-                </div>
-            </div>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      variants={menuSlide}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      className={styles.menu}
+    >
+      <div className={styles.body}>
+        <p>Contact</p>
+        <ContactForm />
+        <div className={styles.nav}>
+          <div className={styles.header}>{/* Contact form here */}</div>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
