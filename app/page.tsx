ea00648e7875aca2c145dd512ff88bd7d2c2f1cc
Rafraud <triforce16mail@gmail.com>
2026-05-31 01:46:45 -0600

    update select color

--- app/page.tsx
+++ app/page.tsx
@@ -216,6 +216,7 @@ export default function Home() {
                 style={{
                   color: "#f1f2ff",
                   width: "700px",
+                  '--rhm-rect-active': '#1648eb',
                 }}
                 rectRender={(props, data) => {
                   // if (!data.count) return <rect {...props} />;
