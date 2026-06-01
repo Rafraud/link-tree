2b6004d8333bf367e68e3101bf8c4c5372f65f70
Rafraud <triforce16mail@gmail.com>
2026-06-01 17:12:16 -0600

    add ontouchmove

--- app/page.tsx
+++ app/page.tsx
@@ -210,10 +210,9 @@ export default function Home() {
             community-driven Live Service Rhythm Game! Check out my git contributions for our ongoing updates below. 
           </span>
           <span className="flex justify-center text-center label-text text-gray-200"></span>
-          <div className="w-full overflow-x-auto" onScroll={() => setShowTooltip(false)}>
-            <div className="flex justify-center w-max min-w-full mx-auto" onScroll={() => setShowTooltip(false)}>
+          <div className="w-full overflow-x-auto" onScroll={() => setShowTooltip(false)} onTouchMove={() => setShowTooltip(false)}>
+            <div className="flex justify-center w-max min-w-full mx-auto">
               <HeatMap
-                onScroll={() => setShowTooltip(false)}
                 value={value}
                 startDate={new Date("2025/06/01")}
                 style={{
