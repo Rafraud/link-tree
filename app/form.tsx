766538bdda6b430f917641fa320ad6a172dea900
Rafraud <triforce16mail@gmail.com>
2026-06-01 12:19:16 -0600

    fontsize

--- app/form.tsx
+++ app/form.tsx
@@ -53,7 +53,7 @@ export const ContactForm = () => {
             <span className="label-text">Name</span>
           </label>
           <input
-            className="input w-full input-bordered input-primary mb-2 mt-1 text-gray-900 bg-gray-200 rounded-md px-1"
+            className="input w-full text-sm input-bordered input-primary mb-2 mt-1 text-gray-900 bg-gray-200 rounded-md px-1"
             value={name}
             onChange={(e) => setName(e.target.value)}
             type="text"
@@ -65,7 +65,7 @@ export const ContactForm = () => {
             <span className="label-text">Email</span>
           </label>
           <input
-            className="input w-full input-bordered input-primary mb-2 mt-1 text-gray-900 bg-gray-200 rounded-md px-1 placeholder:italic placeholder:text-sm"
+            className="input w-full text-sm input-bordered input-primary mb-2 mt-1 text-gray-900 bg-gray-200 rounded-md px-1 placeholder:italic placeholder:text-sm"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             type="email"
@@ -79,7 +79,7 @@ export const ContactForm = () => {
           <div>
             <textarea
               name="contact"
-              className="textarea w-full h-full textarea-primary mb-2 mt-1 text-gray-900 bg-gray-200 rounded-md px-1 placeholder:italic placeholder:text-sm"
+              className="textarea w-full text-sm h-full textarea-primary mb-2 mt-1 text-gray-900 bg-gray-200 rounded-md px-1 placeholder:italic placeholder:text-sm"
               maxLength={500}
               value={message}
               onChange={(e) => setMessage(e.target.value)}
