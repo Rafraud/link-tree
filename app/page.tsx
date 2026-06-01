df6cd0b794436605f84109e2117845d17c2ae13e
Rafraud <triforce16mail@gmail.com>
2026-06-01 12:04:55 -0600

    highlight

--- app/page.tsx
+++ app/page.tsx
@@ -215,7 +215,7 @@ export default function Home() {
                 style={{
                   color: "#f1f2ff",
                   width: "700px",
-                  "--rhm-rect-active": '#1648eb',
+                  "--rhm-rect-active": '#004299',
                 } as React.CSSProperties}
                 rectRender={(props, data) => {
                   // if (!data.count) return <rect {...props} />;
