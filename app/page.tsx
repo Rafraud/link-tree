17e76dc0b2009b23c009cbaf6edacb14fbc2373d
Rafraud <triforce16mail@gmail.com>
2026-06-01 18:41:37 -0600

    warpping tool fix

--- app/page.tsx
+++ app/page.tsx
@@ -236,7 +236,7 @@ export default function Home() {
                       style={{whiteSpace: "nowrap", maxWidth: "none"}}
                       visible={showTooltip}
                       onVisibleChange={(visible) => setShowTooltip(visible)}
-                      autoAdjustOverflow={true}
+                      autoAdjustOverflow={false}
                       content={data.count ? 
                         `${data.count} ${data.count > 1 ? "contributions" : "contribution"} - ${formattedDate.toDateString().split(' ').slice(1).join(' ')}` :
                         `No contributions - ${formattedDate.toDateString().split(' ').slice(1).join(' ')}` }>
